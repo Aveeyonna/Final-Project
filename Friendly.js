14425237e90fcd4d07adb1a6b79d0a7083103b91
Aveeyonna <53492795+Aveeyonna@users.noreply.github.com>
2019-08-19 10:14:47 -0400

    Add files via upload

---
+++ Friendly.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+  /* fade in / fade out effect between Log In / Sign Up forms  */
+  var $signUp = $('.signUp');
+  var $signIn = $('.signIn');
+  $(".btn-member").click(function () {
+    $signUp.fadeOut('slow');
+    $signIn.fadeIn('slow');
+  });
+  $(".btn-back").click(function () {
+    $signIn.fadeOut('slow');
+    $signUp.fadeIn('slow');
+  });
+});
